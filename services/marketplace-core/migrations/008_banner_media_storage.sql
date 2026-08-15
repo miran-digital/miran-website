6982466c30ecb2ca90f126caff643a63f44b7d32
miran-digital <mirandanshvar@gmail.com>
2026-08-16 00:46:28 +0330

    refactor(db): avoid unused banner storage columns

--- services/marketplace-core/migrations/008_banner_media_storage.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE storefront_banners ADD COLUMN storage_key TEXT;
-ALTER TABLE storefront_banners ADD COLUMN mime_type TEXT;
-ALTER TABLE storefront_banners ADD COLUMN size_bytes INTEGER;
-ALTER TABLE storefront_banners ADD COLUMN sha256 TEXT;
-
-CREATE UNIQUE INDEX storefront_banners_storage_key_uq
-  ON storefront_banners(storage_key);
